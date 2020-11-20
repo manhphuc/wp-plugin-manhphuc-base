2ffbab6f452a05a3a51c0c0207d01cd168debc7e
Phuc Nguyen <manhphucofficial@gmail.com>
2020-11-20 12:48:16 +0700

    Change domain author url

--- manhphuc-base.php
+++ manhphuc-base.php
@@ -4,7 +4,7 @@
  * Description: The Base plugin for Theme and Plugin development. It requires ACF pro to work.
  * Version: 0.0.1
  * Author: manhphucofficial@gmail.com
- * Author URI: http://www.hihiweb.com/wordpress-plugin-manhphuc-base
+ * Author URI: http://www.yivic.com/wordpress-plugin-manhphuc-base
  * License: GPLv2 or later
  * Text Domain: manhphuc
  * Domain Path: /languages/
