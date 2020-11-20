9c5547f211cab60b50b7df6db12973b1ced9e3d2
Phuc Nguyen <manhphucofficial@gmail.com>
2020-11-20 12:46:18 +0700

    Init app

---
+++ manhphuc-base.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: ManhPhuc Base
+ * Description: The Base plugin for Theme and Plugin development. It requires ACF pro to work.
+ * Version: 0.0.1
+ * Author: manhphucofficial@gmail.com
+ * Author URI: http://www.hihiweb.com/wordpress-plugin-manhphuc-base
+ * License: GPLv2 or later
+ * Text Domain: manhphuc
+ * Domain Path: /languages/
+ */
